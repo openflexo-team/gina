00b3701a38802c217a72302a951027a3d3680440
Sylvain Guérin <sylvain@openflexo.org>
2015-02-26 23:20:03 +0100

    LOW / Compile issue

--- gina-swing-editor/src/test/java/org/openflexo/fib/testutils/FIBInspectorTestCase.java
+++ gina-swing-editor/src/test/java/org/openflexo/fib/testutils/FIBInspectorTestCase.java
@@ -48,7 +48,7 @@ import java.util.regex.Pattern;
 import org.openflexo.fib.FIBLibrary;
 import org.openflexo.fib.model.FIBComponent;
 import org.openflexo.fib.model.FIBModelFactory;
-import org.openflexo.fib.swing.toolbox.JFIBInspector;
+import org.openflexo.fib.utils.FIBInspector;
 import org.openflexo.fib.utils.GenericFIBTestCase;
 import org.openflexo.model.exceptions.ModelDefinitionException;
 import org.openflexo.model.validation.ValidationError;
@@ -70,7 +70,7 @@ public abstract class FIBInspectorTestCase extends GenericFIBTestCase {
 
 	static {
 		try {
-			INSPECTOR_FACTORY = new FIBModelFactory(JFIBInspector.class);
+			INSPECTOR_FACTORY = new FIBModelFactory(FIBInspector.class);
 		} catch (ModelDefinitionException e1) {
 			e1.printStackTrace();
 		}
