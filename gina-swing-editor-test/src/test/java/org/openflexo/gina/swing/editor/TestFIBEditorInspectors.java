783d73e30a54bd3be84b898f250461be6b9cc1b1
Sylvain Guérin <sylvain@openflexo.org>
2017-04-25 15:22:31 +0200

    MEDIUM / Fixed unit tests

--- gina-swing-editor-test/src/test/java/org/openflexo/gina/swing/editor/TestFIBEditorInspectors.java
+++ gina-swing-editor-test/src/test/java/org/openflexo/gina/swing/editor/TestFIBEditorInspectors.java
@@ -51,158 +51,364 @@ import org.openflexo.rm.ResourceLocator;
 public class TestFIBEditorInspectors extends FIBInspectorTestCase {
 
 	public static void main(String[] args) {
-		System.out.println(generateInspectorTestCaseClass(ResourceLocator.getResourceLocator().locateResource("EditorInspectors"),
-				"EditorInspectors/"));
+		System.out.println("Dir=" + ResourceLocator.getResourceLocator().locateResource("Inspectors"));
+		System.out
+				.println(generateInspectorTestCaseClass(ResourceLocator.getResourceLocator().locateResource("Inspectors"), "Inspectors/"));
 	}
 
 	@Test
-	public void testFIBBrowserInspector() {
-		validateFIB("EditorInspectors/FIBBrowser.inspector");
+	public void testFIBComponentAdvancedInspector() {
+		validateFIB("Inspectors/Advanced/FIBComponent.inspector");
 	}
 
 	@Test
-	public void testFIBButtonInspector() {
-		validateFIB("EditorInspectors/FIBButton.inspector");
+	public void testFIBModelObjectAdvancedInspector() {
+		validateFIB("Inspectors/Advanced/FIBModelObject.inspector");
 	}
 
 	@Test
-	public void testFIBCheckBoxInspector() {
-		validateFIB("EditorInspectors/FIBCheckBox.inspector");
+	public void testFIBWidgetAdvancedInspector() {
+		validateFIB("Inspectors/Advanced/FIBWidget.inspector");
 	}
 
 	@Test
-	public void testFIBCheckboxListInspector() {
-		validateFIB("EditorInspectors/FIBCheckboxList.inspector");
+	public void testFIBBrowserBasicInspector() {
+		validateFIB("Inspectors/Basic/FIBBrowser.inspector");
 	}
 
 	@Test
-	public void testFIBColorInspector() {
-		validateFIB("EditorInspectors/FIBColor.inspector");
+	public void testFIBBrowserActionBasicInspector() {
+		validateFIB("Inspectors/Basic/FIBBrowserAction.inspector");
 	}
 
 	@Test
-	public void testFIBComponentInspector() {
-		validateFIB("EditorInspectors/FIBComponent.inspector");
+	public void testFIBBrowserDragOperationBasicInspector() {
+		validateFIB("Inspectors/Basic/FIBBrowserDragOperation.inspector");
 	}
 
 	@Test
-	public void testFIBContainerInspector() {
-		validateFIB("EditorInspectors/FIBContainer.inspector");
+	public void testFIBBrowserElementBasicInspector() {
+		validateFIB("Inspectors/Basic/FIBBrowserElement.inspector");
 	}
 
 	@Test
-	public void testFIBCustomInspector() {
-		validateFIB("EditorInspectors/FIBCustom.inspector");
+	public void testFIBBrowserElementChildrenBasicInspector() {
+		validateFIB("Inspectors/Basic/FIBBrowserElementChildren.inspector");
 	}
 
 	@Test
-	public void testFIBDropDownInspector() {
-		validateFIB("EditorInspectors/FIBDropDown.inspector");
+	public void testFIBButtonBasicInspector() {
+		validateFIB("Inspectors/Basic/FIBButton.inspector");
 	}
 
 	@Test
-	public void testFIBEditorInspector() {
-		validateFIB("EditorInspectors/FIBEditor.inspector");
+	public void testFIBCheckBoxBasicInspector() {
+		validateFIB("Inspectors/Basic/FIBCheckBox.inspector");
 	}
 
 	@Test
-	public void testFIBFileInspector() {
-		validateFIB("EditorInspectors/FIBFile.inspector");
+	public void testFIBCheckboxListBasicInspector() {
+		validateFIB("Inspectors/Basic/FIBCheckboxList.inspector");
 	}
 
 	@Test
-	public void testFIBFontInspector() {
-		validateFIB("EditorInspectors/FIBFont.inspector");
+	public void testFIBColorBasicInspector() {
+		validateFIB("Inspectors/Basic/FIBColor.inspector");
 	}
 
 	@Test
-	public void testFIBHtmlEditorInspector() {
-		validateFIB("EditorInspectors/FIBHtmlEditor.inspector");
+	public void testFIBComponentBasicInspector() {
+		validateFIB("Inspectors/Basic/FIBComponent.inspector");
 	}
 
 	@Test
-	public void testFIBImageInspector() {
-		validateFIB("EditorInspectors/FIBImage.inspector");
+	public void testFIBContinuousPolarFunctionGraphBasicInspector() {
+		validateFIB("Inspectors/Basic/FIBContinuousPolarFunctionGraph.inspector");
 	}
 
 	@Test
-	public void testFIBLabelInspector() {
-		validateFIB("EditorInspectors/FIBLabel.inspector");
+	public void testFIBContinuousSimpleFunctionGraphBasicInspector() {
+		validateFIB("Inspectors/Basic/FIBContinuousSimpleFunctionGraph.inspector");
 	}
 
 	@Test
-	public void testFIBListInspector() {
-		validateFIB("EditorInspectors/FIBList.inspector");
+	public void testFIBCustomBasicInspector() {
+		validateFIB("Inspectors/Basic/FIBCustom.inspector");
 	}
 
 	@Test
-	public void testFIBModelObjectInspector() {
-		validateFIB("EditorInspectors/FIBModelObject.inspector");
+	public void testFIBDiscretePolarFunctionGraphBasicInspector() {
+		validateFIB("Inspectors/Basic/FIBDiscretePolarFunctionGraph.inspector");
 	}
 
 	@Test
-	public void testFIBMultipleValuesInspector() {
-		validateFIB("EditorInspectors/FIBMultipleValues.inspector");
+	public void testFIBDiscreteSimpleFunctionGraphBasicInspector() {
+		validateFIB("Inspectors/Basic/FIBDiscreteSimpleFunctionGraph.inspector");
 	}
 
 	@Test
-	public void testFIBNumberInspector() {
-		validateFIB("EditorInspectors/FIBNumber.inspector");
+	public void testFIBDiscreteTwoLevelsPolarFunctionGraphBasicInspector() {
+		validateFIB("Inspectors/Basic/FIBDiscreteTwoLevelsPolarFunctionGraph.inspector");
 	}
 
 	@Test
-	public void testFIBPanelInspector() {
-		validateFIB("EditorInspectors/FIBPanel.inspector");
+	public void testFIBDropDownBasicInspector() {
+		validateFIB("Inspectors/Basic/FIBDropDown.inspector");
 	}
 
 	@Test
-	public void testFIBRadioButtonListInspector() {
-		validateFIB("EditorInspectors/FIBRadioButtonList.inspector");
+	public void testFIBEditorBasicInspector() {
+		validateFIB("Inspectors/Basic/FIBEditor.inspector");
 	}
 
 	@Test
-	public void testFIBReferencedComponentInspector() {
-		validateFIB("EditorInspectors/FIBReferencedComponent.inspector");
+	public void testFIBFileBasicInspector() {
+		validateFIB("Inspectors/Basic/FIBFile.inspector");
 	}
 
 	@Test
-	public void testFIBSplitPanelInspector() {
-		validateFIB("EditorInspectors/FIBSplitPanel.inspector");
+	public void testFIBFontBasicInspector() {
+		validateFIB("Inspectors/Basic/FIBFont.inspector");
 	}
 
 	@Test
-	public void testFIBTabInspector() {
-		validateFIB("EditorInspectors/FIBTab.inspector");
+	public void testFIBGraphFunctionBasicInspector() {
+		validateFIB("Inspectors/Basic/FIBGraphFunction.inspector");
 	}
 
 	@Test
-	public void testFIBTableInspector() {
-		validateFIB("EditorInspectors/FIBTable.inspector");
+	public void testFIBHtmlEditorBasicInspector() {
+		validateFIB("Inspectors/Basic/FIBHtmlEditor.inspector");
 	}
 
 	@Test
-	public void testFIBTabPanelInspector() {
-		validateFIB("EditorInspectors/FIBTabPanel.inspector");
+	public void testFIBImageBasicInspector() {
+		validateFIB("Inspectors/Basic/FIBImage.inspector");
 	}
 
 	@Test
-	public void testFIBTextAreaInspector() {
-		validateFIB("EditorInspectors/FIBTextArea.inspector");
+	public void testFIBLabelBasicInspector() {
+		validateFIB("Inspectors/Basic/FIBLabel.inspector");
 	}
 
 	@Test
-	public void testFIBTextFieldInspector() {
-		validateFIB("EditorInspectors/FIBTextField.inspector");
+	public void testFIBListBasicInspector() {
+		validateFIB("Inspectors/Basic/FIBList.inspector");
 	}
 
 	@Test
-	public void testFIBTextWidgetInspector() {
-		validateFIB("EditorInspectors/FIBTextWidget.inspector");
+	public void testFIBMultipleValuesBasicInspector() {
+		validateFIB("Inspectors/Basic/FIBMultipleValues.inspector");
 	}
 
 	@Test
-	public void testFIBWidgetInspector() {
-		validateFIB("EditorInspectors/FIBWidget.inspector");
+	public void testFIBNumberBasicInspector() {
+		validateFIB("Inspectors/Basic/FIBNumber.inspector");
+	}
+
+	@Test
+	public void testFIBNumericFunctionBasicInspector() {
+		validateFIB("Inspectors/Basic/FIBNumericFunction.inspector");
+	}
+
+	@Test
+	public void testFIBPolarFunctionGraphBasicInspector() {
+		validateFIB("Inspectors/Basic/FIBPolarFunctionGraph.inspector");
+	}
+
+	@Test
+	public void testFIBRadioButtonListBasicInspector() {
+		validateFIB("Inspectors/Basic/FIBRadioButtonList.inspector");
+	}
+
+	@Test
+	public void testFIBReferencedComponentBasicInspector() {
+		validateFIB("Inspectors/Basic/FIBReferencedComponent.inspector");
+	}
+
+	@Test
+	public void testFIBSimpleFunctionGraphBasicInspector() {
+		validateFIB("Inspectors/Basic/FIBSimpleFunctionGraph.inspector");
+	}
+
+	@Test
+	public void testFIBSingleParameteredGraphBasicInspector() {
+		validateFIB("Inspectors/Basic/FIBSingleParameteredGraph.inspector");
+	}
+
+	@Test
+	public void testFIBTabBasicInspector() {
+		validateFIB("Inspectors/Basic/FIBTab.inspector");
+	}
+
+	@Test
+	public void testFIBTableBasicInspector() {
+		validateFIB("Inspectors/Basic/FIBTable.inspector");
+	}
+
+	@Test
+	public void testFIBTableActionBasicInspector() {
+		validateFIB("Inspectors/Basic/FIBTableAction.inspector");
+	}
+
+	@Test
+	public void testFIBTableColumnBasicInspector() {
+		validateFIB("Inspectors/Basic/FIBTableColumn.inspector");
+	}
+
+	@Test
+	public void testFIBTextAreaBasicInspector() {
+		validateFIB("Inspectors/Basic/FIBTextArea.inspector");
+	}
+
+	@Test
+	public void testFIBTextFieldBasicInspector() {
+		validateFIB("Inspectors/Basic/FIBTextField.inspector");
+	}
+
+	@Test
+	public void testFIBTextWidgetBasicInspector() {
+		validateFIB("Inspectors/Basic/FIBTextWidget.inspector");
+	}
+
+	@Test
+	public void testFIBWidgetBasicInspector() {
+		validateFIB("Inspectors/Basic/FIBWidget.inspector");
+	}
+
+	@Test
+	public void testFIBContainerContainerInspector() {
+		validateFIB("Inspectors/Container/FIBContainer.inspector");
+	}
+
+	@Test
+	public void testFIBPanelContainerInspector() {
+		validateFIB("Inspectors/Container/FIBPanel.inspector");
+	}
+
+	@Test
+	public void testFIBSplitPanelContainerInspector() {
+		validateFIB("Inspectors/Container/FIBSplitPanel.inspector");
+	}
+
+	@Test
+	public void testFIBTabPanelContainerInspector() {
+		validateFIB("Inspectors/Container/FIBTabPanel.inspector");
+	}
+
+	@Test
+	public void testFIBBrowserControlsInspector() {
+		validateFIB("Inspectors/Controls/FIBBrowser.inspector");
+	}
+
+	@Test
+	public void testFIBButtonControlsInspector() {
+		validateFIB("Inspectors/Controls/FIBButton.inspector");
+	}
+
+	@Test
+	public void testFIBEditorControlsInspector() {
+		validateFIB("Inspectors/Controls/FIBEditor.inspector");
+	}
+
+	@Test
+	public void testFIBListControlsInspector() {
+		validateFIB("Inspectors/Controls/FIBList.inspector");
+	}
+
+	@Test
+	public void testFIBTableControlsInspector() {
+		validateFIB("Inspectors/Controls/FIBTable.inspector");
+	}
+
+	@Test
+	public void testFIBTextWidgetControlsInspector() {
+		validateFIB("Inspectors/Controls/FIBTextWidget.inspector");
+	}
+
+	@Test
+	public void testFIBWidgetControlsInspector() {
+		validateFIB("Inspectors/Controls/FIBWidget.inspector");
+	}
+
+	@Test
+	public void testFIBBrowserActionDefinitionInspector() {
+		validateFIB("Inspectors/Definition/FIBBrowserAction.inspector");
+	}
+
+	@Test
+	public void testFIBBrowserElementDefinitionInspector() {
+		validateFIB("Inspectors/Definition/FIBBrowserElement.inspector");
+	}
+
+	@Test
+	public void testFIBComponentDefinitionInspector() {
+		validateFIB("Inspectors/Definition/FIBComponent.inspector");
+	}
+
+	@Test
+	public void testFIBContainerDefinitionInspector() {
+		validateFIB("Inspectors/Definition/FIBContainer.inspector");
+	}
+
+	@Test
+	public void testFIBModelObjectDefinitionInspector() {
+		validateFIB("Inspectors/Definition/FIBModelObject.inspector");
+	}
+
+	@Test
+	public void testFIBTableActionDefinitionInspector() {
+		validateFIB("Inspectors/Definition/FIBTableAction.inspector");
+	}
+
+	@Test
+	public void testFIBTableColumnDefinitionInspector() {
+		validateFIB("Inspectors/Definition/FIBTableColumn.inspector");
+	}
+
+	@Test
+	public void testFIBBrowserFontAndColorsInspector() {
+		validateFIB("Inspectors/FontAndColors/FIBBrowser.inspector");
+	}
+
+	@Test
+	public void testFIBComponentFontAndColorsInspector() {
+		validateFIB("Inspectors/FontAndColors/FIBComponent.inspector");
+	}
+
+	@Test
+	public void testFIBGraphFunctionFontAndColorsInspector() {
+		validateFIB("Inspectors/FontAndColors/FIBGraphFunction.inspector");
+	}
+
+	@Test
+	public void testFIBPanelFontAndColorsInspector() {
+		validateFIB("Inspectors/FontAndColors/FIBPanel.inspector");
+	}
+
+	@Test
+	public void testFIBTableFontAndColorsInspector() {
+		validateFIB("Inspectors/FontAndColors/FIBTable.inspector");
+	}
+
+	@Test
+	public void testFIBTableColumnFontAndColorsInspector() {
+		validateFIB("Inspectors/FontAndColors/FIBTableColumn.inspector");
+	}
+
+	@Test
+	public void testFIBComponentLayoutInspector() {
+		validateFIB("Inspectors/Layout/FIBComponent.inspector");
+	}
+
+	@Test
+	public void testFIBGraphLayoutInspector() {
+		validateFIB("Inspectors/Layout/FIBGraph.inspector");
+	}
+
+	@Test
+	public void testFIBPanelLayoutInspector() {
+		validateFIB("Inspectors/Layout/FIBPanel.inspector");
 	}
 
 }
